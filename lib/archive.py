bd79c8f3a4a5e3d98f9f340abc79adf6305aab27
Olly Parton <mysticdragonfly@hotmail.co.uk>
2013-06-21 22:07:50 +0100

    Change chat to chat_url in archive.py.
    This will make things clearer when we add the archive code.

--- lib/archive.py
+++ lib/archive.py
@@ -3,10 +3,10 @@ from sqlalchemy.orm.exc import NoResultFound
 
 from model import Log, LogPage
 
-def get_or_create_log(redis, mysql, chat):
+def get_or_create_log(redis, mysql, chat_url):
     # Find existing log or create a new one.
     try:
-        log = mysql.query(Log).filter(Log.url==chat).one()
+        log = mysql.query(Log).filter(Log.url==chat_url).one()
         try:
             latest_page_query = mysql.query(LogPage).filter(LogPage.log_id==log.id)
             latest_page = latest_page_query[-1]
@@ -14,7 +14,7 @@ def get_or_create_log(redis, mysql, chat):
         except IndexError:
             latest_page = new_page(mysql, log)
     except NoResultFound:
-        log = Log(url=chat)
+        log = Log(url=chat_url)
         mysql.add(log)
         mysql.flush()
         latest_page = new_page(mysql, log)
@@ -24,15 +24,16 @@ def new_page(mysql, log, last=0):
     new_page_number = last+1
     latest_page = LogPage(log_id=log.id, number=new_page_number, content=u'')
     mysql.add(latest_page)
-    # XXX COMMIT HERE?
+    mysql.flush()
     log.page_count = new_page_number
     return latest_page
 
-def archive_chat(redis, mysql, chat, backlog=0):
-    log, latest_page = get_or_create_log(redis, mysql, chat)
+def archive_chat(redis, mysql, chat_url, backlog=0):
+    # Text
+    log, latest_page = get_or_create_log(redis, mysql, chat_url)
     # XXX MAKE REALLY REALLY REALLY GODDAMN SURE THIS WORKS WITH MINUS NUMBERS
     # XXX FOR GOD'S SAKE
-    lines = redis.lrange('chat.'+chat, 0, -1-backlog)
+    lines = redis.lrange('chat.'+chat_url, 0, -1-backlog)
     for line in lines:
         # Create a new page if the line won't fit on this one.
         #if len(latest_page.content.encode('utf8'))+len(line)>65535:
@@ -44,49 +45,49 @@ def archive_chat(redis, mysql, chat, backlog=0):
     log.time_saved = datetime.datetime.now()
     mysql.commit()
     # Don't delete from redis until we've successfully committed.
-    redis.ltrim('chat.'+chat, len(lines), -1)
+    redis.ltrim('chat.'+chat_url, len(lines), -1)
     return log.id
 
-def delete_chat_session(redis, chat, session_id):
+def delete_chat_session(redis, chat_url, session_id):
 
-    counter = redis.hget('session.'+session_id+'.meta.'+chat, 'counter')
+    counter = redis.hget('session.'+session_id+'.meta.'+chat_url, 'counter')
     pipe = redis.pipeline()
-    pipe.hdel('chat.'+chat+'.counters', counter)
-    pipe.delete('session.'+session_id+'.chat.'+chat)
-    pipe.delete('session.'+session_id+'.meta.'+chat)
-    pipe.srem('session.'+session_id+'.chats', chat)
-    pipe.zrem('chat-sessions', chat+'/'+session_id)
+    pipe.hdel('chat.'+chat_url+'.counters', counter)
+    pipe.delete('session.'+session_id+'.chat.'+chat_url)
+    pipe.delete('session.'+session_id+'.meta.'+chat_url)
+    pipe.srem('session.'+session_id+'.chats', chat_url)
+    pipe.zrem('chat-sessions', chat_url+'/'+session_id)
     pipe.execute()
 
-def delete_chat(redis, mysql, chat):
+def delete_chat(redis, mysql, chat_url):
 
     # XXX PIPELINE THIS???
 
     # If it's been saved before, save it again before deleting.
-    if redis.hget('chat.'+chat+'.meta', 'type')!='unsaved':
-        archive_chat(redis, mysql, chat, 0)
+    if redis.hget('chat.'+chat_url+'.meta', 'type')!='unsaved':
+        archive_chat(redis, mysql, chat_url, 0)
 
     # Delete metadata first because it's used to check whether a chat exists.
-    redis.delete('chat.'+chat+'.meta')
+    redis.delete('chat.'+chat_url+'.meta')
 
-    redis.delete('chat.'+chat+'.online')
-    redis.delete('chat.'+chat+'.idle')
-    redis.delete('chat.'+chat+'.characters')
+    redis.delete('chat.'+chat_url+'.online')
+    redis.delete('chat.'+chat_url+'.idle')
+    redis.delete('chat.'+chat_url+'.characters')
 
-    for session_id in redis.hvals('chat.'+chat+'.counters'):
-        redis.srem('session.'+session_id+'.chats', chat)
-        redis.delete('session.'+session_id+'.chat.'+chat)
-        redis.delete('session.'+session_id+'.meta.'+chat)
-        redis.zrem('chat-sessions', chat+'/'+session_id)
+    for session_id in redis.hvals('chat.'+chat_url+'.counters'):
+        redis.srem('session.'+session_id+'.chats', chat_url)
+        redis.delete('session.'+session_id+'.chat.'+chat_url)
+        redis.delete('session.'+session_id+'.meta.'+chat_url)
+        redis.zrem('chat-sessions', chat_url+'/'+session_id)
 
-    redis.delete('chat.'+chat+'.counters')
-    redis.delete('chat.'+chat)
-    redis.zrem('delete-queue', chat)
+    redis.delete('chat.'+chat_url+'.counters')
+    redis.delete('chat.'+chat_url)
+    redis.zrem('delete-queue', chat_url)
 
 def delete_session(redis, session_id):
 
     for chat in redis.smembers('session.'+session_id+'.chats'):
-        delete_chat_session(redis, chat, session_id)
+        delete_chat_session(redis, chat_url, session_id)
 
     redis.delete('session.'+session_id)
     redis.delete('session.'+session_id+'.meta')
