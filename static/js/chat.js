11691e102e2382d29b12c981e79c02a0a358fb48
nepeat <nepeat@users.noreply.github.com>
2014-04-10 21:46:46 -0800

    bbcode spoiler fix

--- static/js/chat.js
+++ static/js/chat.js
@@ -597,5 +597,15 @@ $(document).ready(function() {
 	}
 
 	$('#conversation').scrollTop($('#conversation')[0].scrollHeight);
+	$("#textInput").focus();
+
+    $('.spoiler').live('click', function() {
+        if ($(this).css('opacity') == '0') {
+            $(this).css('opacity','1');
+        } else {
+            $(this).css('opacity','0');
+        }
+    });
+
 });
 
