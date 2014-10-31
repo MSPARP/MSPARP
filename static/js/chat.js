5b36e0c9c19556cd519f00ee0cb02aa7e9c4f89a
epithetic <wellingtonvk@mail.com>
2014-10-31 04:35:14 +0000

    missed setting variable

--- static/js/chat.js
+++ static/js/chat.js
@@ -65,6 +65,7 @@ $(document).ready(function() {
 
 	$('body').addClass(localStorage.hdpi);
 	
+	viewport = $("meta[name=viewport]");
 	viewport.setAttribute('content', 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0');
 	
 	if (localStorage.nocreppy == 'nocreppy'){
