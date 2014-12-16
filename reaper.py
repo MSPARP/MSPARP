d4e2f02f119d1433075c72fae3dccc2465d05c81
Olly Parton <mysticdragonfly@hotmail.co.uk>
2014-12-16 19:50:43 +0000

    Split reaping and archiving into separate processes.

--- reaper.py
+++ reaper.py
@@ -23,8 +23,6 @@ if __name__=='__main__':
 
     redis = Redis(host=os.environ['REDIS_HOST'], port=int(os.environ['REDIS_PORT']), db=int(os.environ['REDIS_DB']))
 
-    current_time = datetime.datetime.now()
-
     while True:
 
         for dead in redis.zrangebyscore('chats-alive', 0, get_time()):
@@ -45,42 +43,6 @@ if __name__=='__main__':
         # Send blank messages to avoid socket timeouts.
         for chat in redis.zrangebyscore('longpoll-timeout', 0, get_time()):
             send_message(redis, chat, -1, "message")
-
-        new_time = datetime.datetime.now()
-
-        # Every minute
-        if new_time.minute!=current_time.minute:
-            mysql = sm()
-            
-            # Expire IP bans.
-            redis.zremrangebyscore('ip-bans', 0, get_time())
-
-            # Archive chats.
-            for chat in redis.zrangebyscore('archive-queue', 0, get_time()):
-                archive_chat(redis, mysql, chat, 50)
-                online = redis.scard('chat.'+chat+'.online')
-                # Stop archiving if no-one is online any more.
-                if online == 0:
-                    redis.zrem('archive-queue', chat)
-                else:
-                    redis.zadd('archive-queue', chat, get_time(ARCHIVE_PERIOD))
-
-            # Delete chat-sessions.
-            for chat_session in redis.zrangebyscore('chat-sessions', 0, get_time()):
-                delete_chat_session(redis, *chat_session.split('/'))
-
-            # Delete chats.
-            for chat in redis.zrangebyscore('delete-queue', 0, get_time()):
-                delete_chat(redis, mysql, chat)
-
-            # Delete sessions.
-            for session_id in redis.zrangebyscore('all-sessions', 0, get_time()):
-                delete_session(redis, session_id)
-
-            mysql.close()
-            del mysql
-
-        current_time = new_time
 
         time.sleep(1)
 
