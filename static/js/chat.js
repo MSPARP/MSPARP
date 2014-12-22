20a12c8a720136b31dfbf8b886bf8d1b82a35276
nepeat <nepeat@users.noreply.github.com>
2014-12-21 16:42:37 -0900

    remove the images

--- static/js/chat.js
+++ static/js/chat.js
@@ -101,12 +101,6 @@ $(document).ready(function() {
         hidesystem = localStorage.getItem(chat+"hidesystem");
 
 	}
-	
-	// Let's hook the randomised sidebar in here 8ecause them's the 8r8ks
-	var rnjesus = Math.floor((Math.random() * 100) + 1);
-	if (rnjesus < 45){ $('.sidebar').addClass("kringle"); }
-	else if (rnjesus < 90){ $('.sidebar').addClass("manorah"); }
-	else { $('.sidebar').addClass("geromy"); }
 
     if (hidesystem == 1) {
         $('.hidesystem').attr('checked','checked');
